9a3504944f511a19ec07d095fccc60361ae36a55
bapti <bapti@DESKTOP-BOCDOIU.education.lakanal.epf.fr>
2022-10-25 10:52:56 +0200

    Fin de la classe

--- TP2_convertisseurObjet_BOISSET/src/tp2_convertisseurobjet_boisset/Convertisseur.java
+++ TP2_convertisseurObjet_BOISSET/src/tp2_convertisseurobjet_boisset/Convertisseur.java
@@ -10,4 +10,48 @@ package tp2_convertisseurobjet_boisset;
  */
 public class Convertisseur {
     
+    int nbConversions  ;
+            
+    public Convertisseur () {
+        nbConversions = 0 ;
+    }
+    
+    // fonctions de convertions
+    public double CelciusVersKelvin (double Celcius) {
+    nbConversions++ ;
+    return Celcius += 274.15;
+    }
+   
+    public double KelvinVersCelcius (double Kelvin) {
+    nbConversions++ ;
+    return Kelvin -= 274.15;
+    }
+    
+    public double FarenheitVersCelcius (double Farenheit) {
+    nbConversions++ ;
+    return Farenheit = ( Farenheit - 32 ) / 1.8;
+    }
+    
+    public double CelciusVersFarenheit (double Celcius) {
+    nbConversions=nbConversions+1;
+    return Celcius = (Celcius*1.8) + 32;
+    }
+    
+        
+    public double KelvinVersFarenheit (double Kelvin) {
+    nbConversions--;
+    return Kelvin = CelciusVersFarenheit(KelvinVersCelcius(Kelvin));
+    }
+    
+    public double FarenheitVersKelvin (double Farenheit) {
+    return Farenheit = FarenheitVersCelcius (CelciusVersKelvin(Farenheit));
+    }
+    
+        
+    @Override 
+    public String toString () { 
+        return "nb de conversions "+ nbConversions;
+    }
 }
+    
+    
