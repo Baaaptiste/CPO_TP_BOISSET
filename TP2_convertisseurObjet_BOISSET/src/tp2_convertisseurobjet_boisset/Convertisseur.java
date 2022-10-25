f4747a107a50a4a35ab34fd38defe6e67ccc784a
bapti <bapti@DESKTOP-BOCDOIU.education.lakanal.epf.fr>
2022-10-25 11:39:00 +0200

    Version Final/Comment

--- TP2_convertisseurObjet_BOISSET/src/tp2_convertisseurobjet_boisset/Convertisseur.java
+++ TP2_convertisseurObjet_BOISSET/src/tp2_convertisseurobjet_boisset/Convertisseur.java
@@ -16,6 +16,7 @@ public class Convertisseur {
         nbConversions = 0 ;
     }
     
+    
     // fonctions de convertions
     public double CelciusVersKelvin (double Celcius) {
     nbConversions++ ;
