eb8d16cccfe9530a127f54ae5d9a60d83984502f
bapti <bapti@DESKTOP-BOCDOIU.education.lakanal.epf.fr>
2022-10-25 11:55:35 +0200

    Creation des classes

---
+++ TP2_manip_BOISSET/src/tp2_manip_boisset/Tartiflette.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_manip_boisset;
+
+/**
+ *
+ * @author bapti
+ */
+public class Tartiflette {
+    
+    int nbCalories;
+
+    public Tartiflette (double DesCalories){
+        nbCalories = (int) DesCalories;
+    }
+}
